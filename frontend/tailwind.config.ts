b532fc6e3ed0c9b6614d9e4598885bf3f2ed9110
Aaron William Po <apo2@uwo.ca>
2024-03-07 19:55:46 -0500

    Add frontend files and configurations

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+    require("@headlessui/tailwindcss"),
+    require("tailwindcss-animated"),
+  ],
+} satisfies Config;
